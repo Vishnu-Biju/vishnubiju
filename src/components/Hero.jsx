bc96bc91d603a58cd6d6eaaf8e9d634779e9913e
vishnu biju <vishnubiju.in@gmail.com>
2023-05-12 16:54:00 +0530

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop Mern Stack applications <br className='sm:block hidden' />
-            and reasponsive web applications
+            user interfaces and web applications
           </p>
         </div>
       </div>
